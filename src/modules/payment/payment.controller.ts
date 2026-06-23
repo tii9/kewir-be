2e431dc443cb9eaedc023b8dac66d888c55a22d9
tii9 <christiansean1412@gmail.com>
2026-06-23 11:50:03 +0700

    add vercel config

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,26 +1,22 @@
-import { snap } from '../../lib/midtrans'
-import { Request, Response } from 'express'
+import { snap } from "../../lib/midtrans";
+import { Request, Response } from "express";
 
 export const createCheckout = async (req: Request, res: Response) => {
-    try {
-       
-        const parameter = {
-            transaction_details: {
-                order_id: `ORDER-${Date.now()}`, 
-                gross_amount: 50000 
-            }
-        };
+  try {
+    const parameter = {
+      transaction_details: {
+        order_id: `ORDER-${Date.now()}`,
+        gross_amount: 50000,
+      },
+    };
 
- 
-        const transaction = await snap.createTransaction(parameter);
+    const transaction = await snap.createTransaction(parameter);
 
- 
-        res.status(200).json({
-            token: transaction.token
-        });
-
-    } catch (error) {
-        console.error('Midtrans Error:', error);
-        res.status(500).json({ error: 'Failed to create transaction' });
-    }
+    res.status(200).json({
+      token: transaction.token,
+    });
+  } catch (error) {
+    console.error("Midtrans Error:", error);
+    res.status(500).json({ error: "Failed to create transaction" });
+  }
 };
